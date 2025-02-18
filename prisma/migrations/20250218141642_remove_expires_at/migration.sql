e545236290886a22699cea30bb57a102bb3b0e36
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-02-18 11:18:58 -0300

    fix migration

---
+++ prisma/migrations/20250218141642_remove_expires_at/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `expiresAt` on the `ReadPost` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ReadPost" DROP COLUMN "expiresAt";
